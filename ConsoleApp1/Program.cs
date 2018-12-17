9ce573e1021896c748db1c14b81179df80929ee8
MickaelHm <harmeau.mickael@gmail.com>
2018-12-17 14:31:27 +0100

    Exercice 1 complet

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(a+b);
+        }
+    }
+}
